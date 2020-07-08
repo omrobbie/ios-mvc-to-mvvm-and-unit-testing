d45ccde660514d44f1bbde82e7aee6a5ea646fca
omrobbie <putu.roby@yahoo.com>
2020-07-08 14:35:34 +0800

    Initial Commit

---
+++ MVC to MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC to MVVM
+//
+//  Created by omrobbie on 08/07/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
